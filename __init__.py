f5abf94f731296c3c3e389b6601c5ee38bb93fb2
VaitoSoi <duyduy15456@gmail.com>
2024-07-14 23:36:04 +0700

    Fix bug and Add import error with git submodule

--- __init__.py
+++ __init__.py
@@ -1,7 +1,20 @@
-from db import Problems, Submissions
-from jugde import JugdeMode, JudgeResult, Limit, Language, Compiler
 import os
 import shutil
+
+# Missing data handler
+if not os.path.exists("data"):
+    shutil.copytree("default", "data")
+else:
+    for file in os.listdir("default"):
+        if not os.path.exists(f"data/{file}"):
+            shutil.copy(f"default/{file}", f"data/{file}")
+
+try:
+    from .db import Problems, Submissions
+    from .jugde import JugdeMode, JudgeResult, Limit, Language, Compiler
+except ImportError:
+    from db import Problems, Submissions
+    from jugde import JugdeMode, JudgeResult, Limit, Language, Compiler
 
 __all__ = [
     "Problems",
@@ -10,12 +23,5 @@ __all__ = [
     "JudgeResult",
     "Limit",
     "Language",
-    "Compiler"
+    "Compiler",
 ]
-
-if not os.path.exists("data"):
-    shutil.copytree("default", "data")
-else:
-    for file in os.listdir("default"):
-        if not os.path.exists(f"data/{file}"):
-            shutil.copy(f"default/{file}", f"data/{file}")
--- jugde.py
+++ jugde.py
@@ -27,10 +27,10 @@ class BaseFile(pydantic.BaseModel):
 
 TestType = typing.Literal["file", "std"]
 Language: typing.List[str] = utils.read_json("data/language.json")
-Compiler = utils.read_json("data/file.json")
+Compiler = utils.read_json("data/compiler.json")
 Compiler = { key: BaseCompiler(**value) for key, value in Compiler.items() }
 Compiler = enum.Enum("Compiler", Compiler)
-File = utils.read_json("data/problems.json")
+File = utils.read_json("data/file.json")
 File = { key: BaseFile(**value) for key, value in File.items() }
 File = enum.Enum("File", File)
 
